4c549121df14a84dff81e153cc0afdb46f267a4c
Efecan Altay <efecan95.gs@gmail.com>
2020-07-27 16:17:54 +0300

    Proje dosyası ekle.

---
+++ ReadReceipt/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms;
+using ReadReceipt.Services;
+using ReadReceipt.Views;
+using Plugin.Media;
+
+namespace ReadReceipt
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+            DependencyService.Register<MockDataStore>();
+
+            CrossMedia.Current.Initialize();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ReadReceipt/Views/CameraPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Plugin.Media;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ReadReceipt.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CameraPage : ContentPage
+    {
+        public CameraPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Take_Receipt_Clicked(object sender, System.EventArgs e)
+        {
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+                return;
+            }
+
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+
+            if (photo != null)
+            {
+                cameraStream.Source = ImageSource.FromStream(() =>
+                {
+                    return photo.GetStream();
+                });
+            }
+        }
+
+        private void Read_Receipt_Clicked(object sender, System.EventArgs e)
+        {
+            //
+        }
+    }
+}
